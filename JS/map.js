dcdd447963005318fa815b0ea72bcbbed7b451e0
Camilo Cortes <ccortes2@hawk.iit.edu>
2019-04-17 20:28:25 -0500

    added map api

--- JS/map.js
+++ JS/map.js
@@ -1,1 +1,24 @@
-f
+function initMap() {
+	var tower = {lat: 41.879185, lng: -87.635772}    
+    var map = new google.maps.Map(document.getElementById('map'), {
+          center: tower,
+          zoom: 8
+        });
+		
+	var marker = new google.maps.Marker({
+		  position: tower,
+		  map: map,
+		  animation: google.maps.Animation.BOUNCE,
+		  icon: 'images/marker.png'
+	    });
+		
+	var infowindow = new google.maps.InfoWindow({
+		  content: 'Willis Tower: Skydeck'
+	    });
+		
+
+      
+marker.addListener('click', function() {
+		infowindow.open(map, marker);
+	});
+}
